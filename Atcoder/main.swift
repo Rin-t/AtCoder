7d43979fc9e4bf831cb7131a3ab54e702fe7ff5a
Rin-t <rin.t.4921@gmail.com>
2024-01-01 19:27:36 +0900

    ABC086A - Productを解いた

--- Atcoder/main.swift
+++ Atcoder/main.swift
@@ -5,7 +5,33 @@
 //  Created by Rin on 2023/12/30.
 //
 
+// AtCoder Problems
+// https://kenkoooo.com/atcoder#/table/RinTake
+
+// 勉強方法参考記事
+// https://qiita.com/e869120/items/f1c6f98364d1443148b3
+
+// SwiftでAtCoder
+// https://qiita.com/kntkymt/items/4f02c6b90462f354de6d
+
 import Foundation
 
-print("Hello, World!")
+//MARK: - AtCoder Beginners Selection
+// https://atcoder.jp/contests/abs
+
+//MARK: - ABC086A - Product
+//let nums = readLine()!.split(separator: " ").map { Int(String($0))! }
+//let isEven = nums.reduce(1, *) % 2 == 0
+//let output = isEven ? "Even" : "Odd"
+//print(output)
+
+
+//MARK: - PracticeA - Welcome to AtCode
+//let a = Int(readLine()!)!
+//let bc = readLine()!.split(separator: " ").map { Int(String($0))! }
+//let s = readLine()!
+//
+//let sum = a + bc[0] + bc[1]
+//print("\(sum) \(s)")
+
 
